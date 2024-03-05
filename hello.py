24ea16e6cef9a86a9d795a7fd8fa8618af1d0919
Akshit Shubham IXF 8 <akshitshubhammas@gmail.com>
2024-03-05 23:13:30 +0000

    Done

--- hello.py
+++ hello.py
@@ -1,2 +1,25 @@
-import os
-print(os.getcwd())
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+GOOGLE_API_KEY = 'AIzaSyCHC3WMb6Q1iarIYSTc42K-EAxzxDk4B0U'
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# for m in genai.list_models():
+#   if 'generateContent' in m.supported_generation_methods:
+#     print(m.name)
+
+model = genai.GenerativeModel('gemini-1.0-pro-latest')
+
+response = model.generate_content("What is the meaning of life?")
+
