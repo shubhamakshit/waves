1ceb42c42b8c808b358672cad1d23d87a5966048
Akshit Shubham IXF 8 <akshitshubhammas@gmail.com>
2024-03-06 12:36:59 +0000

    s

--- hello.py
+++ hello.py
@@ -1,25 +1,46 @@
-import pathlib
-import textwrap
 
 import google.generativeai as genai
-
-from IPython.display import display
-from IPython.display import Markdown
-
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 GOOGLE_API_KEY = 'AIzaSyCHC3WMb6Q1iarIYSTc42K-EAxzxDk4B0U'
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
-# for m in genai.list_models():
-#   if 'generateContent' in m.supported_generation_methods:
-#     print(m.name)
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
 
-model = genai.GenerativeModel('gemini-1.0-pro-latest')
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
 
-response = model.generate_content("What is the meaning of life?")
+model = genai.GenerativeModel(model_name="gemini-1.0-pro-001",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
+convo = model.start_chat(history=[
+])
+
+while True:
+  x = input(f"|{'OP-Gemini'}>")
+  if x.strip() == "exit": break
+  convo.send_message(x)
+  print(f'\n{convo.last.text}\n')
